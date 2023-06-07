0febf957aaa36121b9f4283a5c8d5c34871f57e0
magdy <mohamedmagdycivily@gmail.com>
2023-06-07 17:43:14 +0300

    fix

--- index.js
+++ index.js
@@ -110,7 +110,9 @@ app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
   jwt.verify(token, secret, {}, async (err,info) => {
     if (err) throw err;
     const {id,title,summary,content} = req.body;
+    console.log('🌟🌟🌟 id = ', id);
     const postDoc = await Post.findById(id);
+    console.log('🌟🌟🌟 postDoc = ', postDoc);
     const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
     if (!isAuthor) {
       return res.status(400).json('you are not the author');
